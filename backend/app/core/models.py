dc675edb8419681c4c88ec0ddbb0f64fc73d2875
mihailkirov <mihailkirovv@gmail.com>
2021-02-21 13:10:51 +0100

    Added information about users 2FA state in display of the user object

--- backend/app/core/models.py
+++ backend/app/core/models.py
@@ -63,7 +63,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
 
     def __str__(self):
-        return  self.username + " " + self.email
+        return  self.username + " " + self.email + " 2FA-" + self.profile.two_factor_enabled
 
 class Profile(models.Model):
     """Profile of a user related to authentication features"""
