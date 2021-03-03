f4c7a07553092b3c8c5f2dbd786fa88652bc0f0c
Maxim Durand <durandmaxime1931@gmail.com>
2021-03-03 19:19:48 +0100

    Finalized local export for frontend

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -13,6 +13,7 @@ import os
 import django
 
 import environ
+
 from django.core.wsgi import get_wsgi_application
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- backend/app/core/models.py
+++ backend/app/core/models.py
@@ -111,6 +111,8 @@ class Diagram(models.Model):
 
     diagramStat = models.ForeignKey(DiagramStat,
                                     on_delete=models.CASCADE)
+    riskTable = models.TextField(default="")
+
 
     def get_tags(self):
         return self.tags.names()
