43465d9f361c43692f83891cea9fb4bc5466489a
cchavano <58697129+cchavano@users.noreply.github.com>
2021-03-08 19:23:06 +0100

    Update the start.sh script

--- start.sh
+++ start.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-if [ $# = 0 ]; then
-    docker-compose up
-elif [ $# = 1 ]; then
-    if [ $1 = "frontend" ]; then
+help_message="USAGE: ./start.sh [app]
+app:
+    <<empty>>: launch the entire app
+    frontend: launch the frontend
+    backend: launch the backend"
+
+if [ "$#" = 0 ]; then
+    docker-compose up 
+elif [ "$#" = 1 ]; then
+    if [ "$1" = "-h" -o "$1" = "--help" ]; then
+        echo "$help_message"
+    elif [ "$1" = "frontend" ]; then
         cd ./frontend; docker-compose up
-    elif [ $1 = "backend" ]; then
+    elif [ "$1" = "backend" ]; then
         cd ./backend; docker-compose up
     else
-        echo "Invalid argument"
+        echo "Illegal argument '$1'"
+        echo "$help_message"
     fi
 else
-    echo "Too many arguments"
+    echo "Illegal arguments"
+    echo "$help_message"
 fi
-
