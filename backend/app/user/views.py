299bca46fe2c92e29b6e7c3311c9f5bc7f8c925c
cchavano <58697129+cchavano@users.noreply.github.com>
2021-03-12 17:25:21 +0100

    Fix some backend errors linked to 2FA disabling

--- backend/app/user/views.py
+++ backend/app/user/views.py
@@ -433,9 +433,9 @@ class DisableTOTP(APIView):
         user = request.user
         device = get_user_totp_device(user, True)
 
-        if not device or device.confirmed:
+        if not device or not device.confirmed:
             logger.warning("Suspicious 2FA desactivation - no 2FA or 2FA not activated for user %s", user)
-            return Response(dict(errors=["Ivalid operation"], status=status.HTTP_400_BAD_REQUEST))
+            return Response(dict(errors=["Ivalid operation"]), status=status.HTTP_400_BAD_REQUEST)
 
         token_totp = request.data["token_totp"]
         if not device.verify_token(token_totp):
