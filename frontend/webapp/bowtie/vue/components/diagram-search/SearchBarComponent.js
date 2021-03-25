30811b53caaa5266fb5d9f1eb5e7980a9c171c91
Maxim Durand <durandmaxime1931@gmail.com>
2021-03-25 22:11:29 +0100

    first try to implement prefix tree for autocompletion

--- frontend/webapp/bowtie/vue/components/diagram-search/SearchBarComponent.js
+++ frontend/webapp/bowtie/vue/components/diagram-search/SearchBarComponent.js
@@ -1,3 +1,80 @@
+
+class Trie{
+    constructor() {
+        this.trie = null;
+        this.suggestions = [];
+    }
+    newNode() {
+        return {
+            isLeaf: false,
+            children: {}
+        }
+    }
+
+    add(word) {
+        if (!this.trie) this.trie = this.newNode();
+
+        let root = this.trie;
+        for (const letter of word) {
+            if (!(letter in root.children)) {
+                root.children[letter] = this.newNode();
+            }
+            root = root.children[letter];
+        }
+        root.isLeaf = true;
+    }
+
+    find(word) {
+        let root = this.trie;
+        for (const letter of word) {
+            if (letter in root.children) {
+                root = root.children[letter];
+            } else {
+                return null;
+            }
+        }
+
+        return root;
+    }
+
+    traverse(root, word) {
+        if (root.isLeaf) {
+            this.suggestions.push(word);
+            return;
+        }
+
+        for (const letter in root.children) {
+            this.traverse(root.children[letter], word + letter);
+        }
+    }
+
+    complete(word, CHILDREN = null) {
+        const root = this.find(word);
+
+        if (!root) return this.suggestions; // cannot suggest anything
+
+        const children = root.children;
+
+        let spread = 0;
+
+        for (const letter in children) {
+            this.traverse(children[letter], word + letter);
+            spread++;
+
+            if (CHILDREN && spread === CHILDREN) break;
+        }
+
+        return this.suggestions;
+    }
+
+    clear() {
+        this.suggestions = [];
+    }
+
+    print() {
+        console.log(this.trie);
+    }
+}
 let SearchBarComponent = {
 
     template: '#search-bar-template',
@@ -5,10 +82,13 @@ let SearchBarComponent = {
     components: {},
     props: { //Link with parent, parent can send data with 'v-bind'. To go from child to parent $emit("param")
         nameInput:String,
+        all_diagrams:Array,
+        tags_selected:Array,
     },
     data: function () {
         return {
             is_public: false,
+            trie: new Trie(),
         }
     },
     methods: {
@@ -20,10 +100,18 @@ let SearchBarComponent = {
 
     },
     computed: {// Sort of augmented variables. Seen by VueJs as variable but are actually func
-        // beforeMount is launched at the creation of the component
-        beforeMount() {
 
+    },
+    // beforeMount is launched at the creation of the component
+    mounted() {
+        for(const diag of this.all_diagrams){
+            this.trie.add(diag.name.toLowerCase())
+            console.log('added'+diag.name)
         }
-    }
+        for(const tag of this.tags_selected){
+            this.trie.add(tag.toLowerCase())
+        }
+
+    },
 
 }
