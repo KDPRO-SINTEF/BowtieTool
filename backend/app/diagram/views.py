dd820cdc4041a1fadf0b0beabf5563106aa60a4e
Maxim Durand <durandmaxime1931@gmail.com>
2021-03-28 18:33:22 +0200

    Fixed saving public diagrams.

--- backend/app/diagram/views.py
+++ backend/app/diagram/views.py
@@ -20,7 +20,6 @@ from django.db.models import Q, Avg, Count, Min, Sum, F
 import PIL
 
 
-
 class IsResearcher(BasePermission):
     """
     Allows access only to researchers.
@@ -69,11 +68,7 @@ class DiagramDetail(APIView):
         else:
             queryset = Diagram.objects.all().filter(Q(owner=self.request.user) | Q(is_public=True))
         try:
-            print(queryset)
-            for i in queryset:
-                print(i.id)
             queryset = queryset.get(pk=pk)
-            print(queryset)
             return queryset
         except Diagram.DoesNotExist:
             raise Http404
@@ -90,16 +85,18 @@ class DiagramDetail(APIView):
         # response['Content-Disposition'] = 'attachment; filename="%s"' % path.split('/')[-1]
         return response
 
-    # TODO: handle case where diagram public , and owner update ( currently duplicates file)
+    # TODO: handle case where diagram public
     def put(self, request, pk):
         """Update diagram"""
         diagramModel = self.get_object(pk)
         serializer = serializers.DiagramSerializer(data=request.data)
         if serializer.is_valid():
-            if diagramModel.is_public:
-                # TODO Verify this work for public diagrams
-                serializer.save(owner=request.user, is_Public=False, lastTimeSpent=request.data['lastTimeSpent'],preview=request.data['preview'])
-                # diagramModel.delete()
+            print(diagramModel.owner.username)
+            print(request.user.username)
+            if diagramModel.is_public and (diagramModel.owner.username != request.user.username):
+                # If current user isn't the owner of the diagram,
+                # then he can only create a new private diagram from the public one
+                serializer.save(owner=request.user, is_public=False, lastTimeSpent=request.data['lastTimeSpent'])
             else:
                 diagramModel.lastTimeSpent = float(request.data['lastTimeSpent'])
                 diagramModel.name = str(request.data['name'])
