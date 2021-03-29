f2a3c7037b33023cc8b3e1f15bf2ac71f53f4af9
cchavano <igccw@laposte.net>
2021-03-29 12:25:04 +0200

    New statistics page

--- frontend/webapp/bowtie/vue/components/stats/StatsItemComponent.js
+++ frontend/webapp/bowtie/vue/components/stats/StatsItemComponent.js
@@ -1,15 +1,23 @@
 /**
  * StatsItemComponent
  * Handles the window to show the statistics analysis
+ * Related template: common/statistics.html
  */
 
 let StatsItemComponent = {
-    template :
-        `<p class="text-center">Average number of {{label}} : {{number}}</p>`,
+    template: '#statistics-item-template',
     props: {
         label : String,
-        number : Number
+        number : Number,
+        imageSrc: String
     },
-    data : function() { return{} },
-
+    methods: {
+        copyValueToClipboard: function() {
+            navigator.clipboard.writeText(this.number)
+                .then(() => this.$emit('value-copied', 'Value has been copied to clipboard.'))
+                .catch(err => {
+                    alert('Error in copying text: ' + err);
+                })
+        }
+    }
 }
