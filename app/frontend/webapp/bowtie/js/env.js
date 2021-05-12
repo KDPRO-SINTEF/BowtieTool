4fdac8efca2e1677ee5ca8d8f6724808bbd66e62
cchavano <58697129+cchavano@users.noreply.github.com>
2021-05-12 23:15:00 +0200

    Fix api port bug in frontend

--- app/frontend/webapp/bowtie/js/env.js
+++ app/frontend/webapp/bowtie/js/env.js
@@ -1,4 +1,4 @@
 const PROTOCOL = 'http';
 const API_SERVER_HOST = 'localhost';
-const API_SERVER_PORT = '80';
+const API_SERVER_PORT = '8000';
 const API_SERVER_URL = PROTOCOL + '://' + API_SERVER_HOST + ':' + API_SERVER_PORT;
